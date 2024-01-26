ad010487480a6acbb0c0bc81e24f337d22ece383
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-01-26 11:32:59 +0100

    feet/E3

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const app = express();
+
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Charger les données du fichier JSON
+const dataPath = path.join(__dirname, 'data', 'articles.json');
+const articlesData = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+app.use(express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index', { articles: articlesData });
 });
 
-app.listen(3000, (req,res) => {
-    console.log(`serveur connecté sur le port ${port}`);
+app.get('/article', (req, res) => {
+  res.render('articles', { articles: articlesData });
+});
+
+app.listen(3000, () => {
+  console.log(`Serveur connecté sur le port ${port}`);
 });
